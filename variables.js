7a09fd585d6f1ac325f60d69c9894a36d4c520df
Raymond Sims <lancelot53.rs@gmail.com>
2026-07-03 08:47:32 -0400

    Add variables JavaScript example

---
+++ variables.js
@@ -0,0 +1,7 @@
+ const name = "Raymond";
+let age = 65;
+const learning = "JavaScript";
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old.");
+console.log("I am learning " + learning);
